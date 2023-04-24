a38be10decf81365bee7406a744cd4b7487e1b22
rb718 <ghisodye@hotmail.com>
2023-04-24 04:55:07 -0500

    only use  syntax if you have the  property set to  in your  file. Otherwise, you only have to use the  syntax.

--- sendgrid_api_send.js
+++ sendgrid_api_send.js
@@ -1,12 +1,12 @@
 "use strict";
 require("dotenv").config();
 
-import { readFileSync } from "fs";
-import { readFile } from "xlsx";
+const fs = require("fs");
+const XLSX = require("xlsx");
 
-const htmlFile = readFileSync("./index.htm", { encoding: "utf-8" });
+const htmlFile = fs.readFileSync("./index.htm", { encoding: "utf-8" });
 
-let workbook = readFile("./emails.xlsx");
+let workbook = XLSX.readFile("./emails.xlsx");
 let sheet_name_list = workbook.SheetNames;
 let emailList = [];
 let xlData = workbook.Sheets[sheet_name_list[0]];
@@ -22,8 +22,8 @@ for (let i = 1; i < xlKeys.length - 1; i++) {
 // using Twilio SendGrid's v3 Node.js Library
 // https://github.com/sendgrid/sendgrid-nodejs
 
-import { setApiKey, send } from "@sendgrid/mail";
-setApiKey(process.env.SENDGRID_API_KEY);
+const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 // async..await is not allowed in global scope, must use a wrapper
 async function main(toEmail) {
@@ -34,7 +34,8 @@ async function main(toEmail) {
     text: process.env.MAIL_TEXT,
     html: htmlFile,
   };
-  send(msg)
+  sgMail
+    .send(msg)
     .then(() => {
       console.log("Email sent");
     })
