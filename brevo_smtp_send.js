de10fc533325ba6eb5f61f79e6bf005462b23858
rb718 <ghisodye@hotmail.com>
2023-11-09 13:07:34 -0600

    debugging

--- brevo_smtp_send.js
+++ brevo_smtp_send.js
@@ -9,40 +9,40 @@ const XLSX = require("xlsx");
 
 // async..await is not allowed in global scope, must use a wrapper
 async function main(toEmail, htmlBody, indexN) {
-	// create reusable transporter object using the default SMTP transport
-	let transporter = nodemailer.createTransport({
-		host: process.env.BREVO_SMTP_SERVER,
-		port: process.env.BREVO_SMTP_PORT,
-		secure: false, // true for 465, false for other ports
-		auth: {
-			user: process.env.BREVO_SMTP_USERNAME,
-			pass: process.env.BREVO_SMTP_PASSWORD,
-		},
-	});
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: process.env.BREVO_SMTP_SERVER,
+    port: process.env.BREVO_SMTP_PORT,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: process.env.BREVO_SMTP_USERNAME,
+      pass: process.env.BREVO_SMTP_PASSWORD,
+    },
+  });
 
-	// send mail with defined transport object
-	let info = await transporter.sendMail({
-		from: process.env.EMAIL_FROM, // sender address
-		to: toEmail, // list of receivers
-		subject: process.env.EMAIL_SUBJECT, // Subject line
-		text: process.env.EMAIL_TEXT, // plain text body
-		// html: htmlBody, // html body
-	});
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: process.env.EMAIL_FROM, // sender address
+    to: toEmail, // list of receivers
+    subject: process.env.EMAIL_SUBJECT, // Subject line
+    text: process.env.EMAIL_TEXT, // plain text body
+    // html: htmlBody, // html body
+  });
 
-	console.log("Message sent: %s", info.messageId);
-	console.log(info.envelope, " => ", indexN)
+  console.log("Message sent: %s", info.messageId);
+  console.log(info.envelope, " => ", indexN);
 }
 
 // const htmlFile = fs.readFileSync("./index.htm", { encoding: "utf-8" });
-const htmlFile = null
+const htmlFile = null;
 
 let workbook = XLSX.readFile(process.env.HOLDERS);
 let sheet_name_list = workbook.SheetNames;
 let xlData = workbook.Sheets[sheet_name_list[0]];
 let xlKeys = Object.keys(xlData);
-
+console.log(xlKeys.length);
 for (let i = 1; i < xlKeys.length - 1; i++) {
-	const key = xlKeys[i];
-	const emailName = xlData[key].v;
-	main(emailName, htmlFile, i).catch(console.error);
+  const key = xlKeys[i];
+  const emailName = xlData[key].v;
+  main(emailName, htmlFile, i).catch(console.error);
 }
