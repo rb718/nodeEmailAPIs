7abf7bec2e82354d77e463787d4f4f17945e6e67
rb718 <ghisodye@hotmail.com>
2023-11-15 18:00:06 -0600

    add receiver name

--- brevo_smtp_send.js
+++ brevo_smtp_send.js
@@ -8,7 +8,7 @@ const fs = require("fs");
 const XLSX = require("xlsx");
 
 // async..await is not allowed in global scope, must use a wrapper
-async function main(toEmail, htmlBody, indexN) {
+async function main(toEmail, toUser, htmlBody, indexN) {
   // create reusable transporter object using the default SMTP transport
   let transporter = nodemailer.createTransport({
     host: process.env.BREVO_SMTP_SERVER,
@@ -20,12 +20,14 @@ async function main(toEmail, htmlBody, indexN) {
     },
   });
 
+  // console.log("Hi " + `${toUser}` + process.env.EMAIL_TEXT);
+  let bodyEmail = "Hi " + `${toUser}` + process.env.EMAIL_TEXT;
   // send mail with defined transport object
   let info = await transporter.sendMail({
     from: process.env.EMAIL_FROM, // sender address
     to: toEmail, // list of receivers
     subject: process.env.EMAIL_SUBJECT, // Subject line
-    text: process.env.EMAIL_TEXT, // plain text body
+    text: bodyEmail, // plain text body
     // html: htmlBody, // html body
   });
 
@@ -41,8 +43,14 @@ let sheet_name_list = workbook.SheetNames;
 let xlData = workbook.Sheets[sheet_name_list[0]];
 let xlKeys = Object.keys(xlData);
 console.log(xlKeys.length);
-for (let i = 1; i < xlKeys.length - 1; i++) {
-  const key = xlKeys[i];
-  const emailName = xlData[key].v;
-  main(emailName, htmlFile, i).catch(console.error);
+let countRow = 0;
+for (let i = 1; i < xlKeys.length - 1; i += 2) {
+  const keyA = xlKeys[i];
+  const emailName = xlData[keyA].v;
+  const keyB = xlKeys[i + 1];
+  const firstName = xlData[keyB].v;
+  countRow += 1;
+  console.log(countRow);
+  // console.log(emailName, firstName);
+  main(emailName, firstName, htmlFile, countRow).catch(console.error);
 }
